69c2226a804820da21f248721f485b4845c8f0f8
olusegun Owolabi <olujawo1996@gmail.com>
2022-10-26 15:05:08 +0100

    source codes

---
+++ script.js
@@ -0,0 +1,26 @@
+const cat_result = document.getElementById("cat_result");
+const dog_result = document.getElementById("dog_result");
+const cat_btn = document.getElementById("cat_btn");
+const dog_btn = document.getElementById("dog_btn");
+
+cat_btn.addEventListener("click", getRandomCat);
+dog_btn.addEventListener("click", getRandomDog);
+
+getRandomCat();
+getRandomDog();
+
+function getRandomCat() {
+  fetch("https://aws.random.cat/meow")
+    .then((res) => res.json()) //result converted to object
+    .then((data) => {
+      cat_result.innerHTML = `<img src = "${data.file}" />`;
+    });
+}
+
+function getRandomDog() {
+  fetch("https://random.dog/woof.json")
+    .then((res) => res.json()) //result converted to object
+    .then((data) => {
+      dog_result.innerHTML = `<img src = "${data.url}" />`;
+    });
+}
